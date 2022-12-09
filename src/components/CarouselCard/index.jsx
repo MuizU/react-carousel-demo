5e60eea6bdf04d7f5269d2d7d1585cd545149ab8
MuizU <muizuvais99@gmail.com>
2022-12-09 22:14:34 +0530

    Made the card more responsive

--- src/components/CarouselCard/index.jsx
+++ src/components/CarouselCard/index.jsx
@@ -1,16 +1,21 @@
-import style from './style.module.sass'
-export default function Card({make,model, image, description, price, level }) {
-    const className = 'level' + level
+import style from "./style.module.sass";
+export default function Card({
+    make,
+    model,
+    image,
+    description,
+    price,
+    level,
+}) {
+    const className = "level" + level;
     return (
-        <div className={style.card + ' '+ style[className]}>
-            <div className={style.card_image}>
-                <img src={image} alt={`${make} ${model}`}/>
-            </div>
-            <div className={style.card_content}>
-                <h3 className={style.description}>{make} {model}</h3>
-                <p className={style.description}>{description}</p>
-                <p>{price}</p>
-            </div>
+        <div className={style.card + " " + style[className]}>
+            <img src={image} alt={`${make} ${model}`} />
+            <h3 className={style.description}>
+                {make} {model}
+            </h3>
+            <p className={style.description}>{description}</p>
+            <p>{price}</p>
         </div>
-    )
+    );
 }
